3003d87871b0bc2e68c0595ff3f920f3f3e76086
viniciusbuffolo <vinicius.buffolo@gmail.com>
2017-02-03 16:46:16 -0200

    Layout_03.02.2017

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('htmlheader_title', 'Home')
+
+@section('main-content')
+    <h1>Porque nós o usamos?</h1>
+    <p>É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,404 @@
+<html>
+    <head>
+        <title>Transparência - @yield('htmlheader_title', '') </title>
+        @section('htmlheader')
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Transparencia - @yield('htmlheader_title')</title>
+            <!-- Tell the browser to be responsive to screen width -->
+            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+            <!-- Bootstrap 3.3.6 -->
+            <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" />
+            <!-- Font Awesome -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+            <!-- Ionicons -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+            <!-- Theme style -->
+            <link rel="stylesheet" href="{{ asset('/dist/css/AdminLTE.min.css') }}">
+            <!-- AdminLTE Skins. Choose a skin from the css/skins
+                folder instead of downloading all of them to reduce the load. -->
+            <link rel="stylesheet" href="{{ asset('/dist/css/skins/_all-skins.min.css') }}">
+            <!-- iCheck -->
+            <link rel="stylesheet" href="{{ asset('/plugins/iCheck/flat/blue.css') }}">
+            <!-- Morris chart -->
+            <link rel="stylesheet" href="{{ asset('/plugins/morris/morris.css') }}">
+            <!-- jvectormap -->
+            <link rel="stylesheet" href="{{ asset('/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+            <!-- Date Picker -->
+            <link rel="stylesheet" href="{{ asset('/plugins/datepicker/datepicker3.css') }}">
+            <!-- Daterange picker -->
+            <link rel="stylesheet" href="{{ asset('/plugins/daterangepicker/daterangepicker.css') }}">
+            <!-- bootstrap wysihtml5 - text editor -->
+            <link rel="stylesheet" href="{{ asset('/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+
+            <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+            <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
+        @show
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="{{ ('/') }}" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>T</b></span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Transparência</b></span>
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            </nav>
+        </header>
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+            <!-- search form -->
+            <form action="#" method="get" class="sidebar-form">
+                <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Pesquisar...">
+                    <span class="input-group-btn">
+                        <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
+            <!-- /.search form -->
+            <!-- Sidebar Menu -->
+				<ul class="sidebar-menu">
+					<li class="header">MENU</li>
+					<!-- Optionally, you can add icons to the links -->
+					<li class="active">
+						<a href="#">
+							<i class="fa fa-home"></i>
+							<span>Home</span>
+						</a>
+					</li>
+					<li class="treeview">
+						<a href="#">
+							<i class="fa fa-info-circle"></i>
+							<span>O Portal</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+						<ul class="treeview-menu">
+							<li>
+								<a href="#">O que é o Portal</a>
+							</li>
+							<li>
+								<a href="#">Glossário</a>
+							</li>
+							<li>
+								<a href="#">Manual de Navegação</a>
+							</li>
+							<li>
+								<a href="#">Legislação</a>
+							</li>
+							<li>
+								<a href="#">Perguntas Frequentes</a>
+							</li>
+							<li>
+								<a href="#">Mapa do Site</a>
+							</li>
+						</ul>
+					</li>
+					<li class="treeview">
+						<a href="#">
+							<i class="fa fa-search"></i>
+							<span>Consultas</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+						<ul class="treeview-menu">
+							<li class="treeview">
+								<a href="#">
+									<span>Receitas e Orçamentos</span>
+									<span class="pull-right-container">
+										<i class="fa fa-angle-left pull-right"></i>
+									</span>
+								</a>
+								<ul class="treeview-menu">
+									<li>
+										<a href="#">Receita</a>
+									</li>
+									<li class="treeview">
+										<a href="#">
+											<span>Leis Orçamentárias</span>
+											<span class="pull-right-container">
+												<i class="fa fa-angle-left pull-right"></i>
+											</span>
+										</a>
+										<ul class="treeview-menu">
+											<li>
+												<a href="#">Plano Plurianual PPA</a>
+											</li>
+											<li>
+												<a href="#">Lei de Diretrizes Orçamentárias LDO</a>
+											</li>
+											<li>
+												<a href="#">Orçamentos</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Orçamento x Executado</a>
+									</li>
+								</ul>
+							</li>
+							<li class="treeview">
+								<a href="#">
+									<span>Despesas e Repasses</span>
+									<span class="pull-right-container">
+										<i class="fa fa-angle-left pull-right"></i>
+									</span>
+								</a>
+								<ul class="treeview-menu">
+									<li>
+										<a href="#">Despesa</a>
+									</li>
+									<li>
+										<a href="#">Saldo de Restos a Pagar</a>
+									</li>
+									<li>
+										<a href="#">Relatório LRF</a>
+									</li>
+									<li>
+										<a href="#">Convênios com Municípios</a>
+									</li>
+									<li>
+										<a href="#">Convênios da União</a>
+									</li>
+									<li>
+										<a href="#">Transferências para Municípios</a>
+									</li>
+									<li>
+										<a href="#">Transferências da União</a>
+									</li>
+									<li>
+										<a href="#">Cartão Corporativo</a>
+									</li>
+									<li>
+										<a href="#">Glossário</a>
+									</li>
+									<li>
+										<a href="#">Despesas com Publicidade </a>
+									</li>
+								</ul>
+							</li>
+							<li class="treeview">
+								<a href="#">
+									<span>Compras</span>
+									<span class="pull-right-container">
+										<i class="fa fa-angle-left pull-right"></i>
+									</span>
+								</a>
+								<ul class="treeview-menu">
+									<li>
+										<a href="#">Compras Governamentais</a>
+									</li>
+									<li>
+										<a href="#">Contratos</a>
+									</li>
+									<li>
+										<a href="#">Patrimônio</a>
+									</li>
+									<li>
+										<a href="#">Portal de Compras</a>
+									</li>
+									<li>
+										<a href="#">Tabelas de Preços Referenciais</a>
+									</li>
+									<li>
+										<a href="#">Empresas Inidôneas e Suspensas</a>
+									</li>
+									<li>
+										<a href="#">Empresas Punidas</a>
+									</li>
+									<li>
+										<a href="#">Fornecedores Sancionados</a>
+									</li>
+								</ul>
+							</li>
+							<li class="treeview">
+								<a href="#">
+									<span>Pessoal</span>
+									<span class="pull-right-container">
+										<i class="fa fa-angle-left pull-right"></i>
+									</span>
+								</a>
+								<ul class="treeview-menu">
+									<li>
+										<a href="#">Servidores Públicos</a>
+									</li>
+									<li>
+										<a href="#">Diárias por Orgão</a>
+									</li>
+									<li>
+										<a href="#">Diárias por favorecido</a>
+									</li>
+									<li>
+										<a href="#">Jetons por favorecido</a>
+									</li>
+									<li>
+										<a href="#">Diárias da Polícia Militar</a>
+									</li>
+								</ul>
+							</li>
+							<li class="treeview">
+								<a href="#">
+									<span>Outras Consultas</span>
+									<span class="pull-right-container">
+										<i class="fa fa-angle-left pull-right"></i>
+									</span>
+								</a>
+								<ul class="treeview-menu">
+									<li>
+										<a href="#">Diário Oficial</a>
+									</li>
+									<li>
+										<a href="#">Prestação de Contas do Governador</a>
+									</li>
+									<li>
+										<a href="#">Fluxo de Veículos na Rodovia do Sol</a>
+									</li>
+									<li>
+										<a href="#">Empresas Públicas e Outros Poderes</a>
+									</li>
+									<li>
+										<a href="#">Conselhos Estaduais</a>
+									</li>
+									<li>
+										<a href="#">Monte sua consulta</a>
+									</li>
+									<li>
+										<a href="#">Relatório de Atividades CPCT</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li class="treeview">
+						<a href="#">
+							<i class="fa fa-university"></i>
+							<span>Áreas Temáticas</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+						<ul class="treeview-menu">
+							<li>
+								<a href="#">Educação</a>
+							</li>
+							<li>
+								<a href="#">Obras</a>
+							</li>
+							<li>
+								<a href="#">Saúde</a>
+							</li>
+							<li>
+								<a href="#">Segurança</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-map"></i>
+							<span>Mapas</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-folder-open"></i>
+							<span>Dados Abertos</span>
+						</a>
+					</li>
+				</ul>
+				<!-- /.sidebar-menu -->
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+            <h1>
+                Painel
+                <small>Transparência</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <!--<li class="active">Dashboard</li>-->
+            </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                @yield('main-content')
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <footer class="main-footer">
+            <strong>
+				&copy; 2016
+				<a href="#">Controladoria</a>.
+			</strong>
+			Rua Brahim Antônio Seder, 96/102, Centro - CEP: 29300060 - Cachoeiro de Itapemirim / ES.
+			<br />
+			Tel: (0xx28) 3155-5237 / Ouvidoria: 156 / E-mail: controladoria@cachoeiro.es.gov.br
+        </footer>
+        <!-- ./wrapper -->
+
+        @section('scripts')
+            <!-- jQuery 2.2.3 -->
+            <script src="{{ asset('/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+            <!-- jQuery UI 1.11.4 -->
+            <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+            <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+            <script>
+            $.widget.bridge('uibutton', $.ui.button);
+            </script>
+            <!-- Bootstrap 3.3.6 -->
+            <script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}"></script>
+            <!-- Morris.js charts -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+            <script src="{{ asset('/plugins/morris/morris.min.js') }}"></script>
+            <!-- Sparkline -->
+            <script src="{{ asset('/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+            <!-- jvectormap -->
+            <script src="{{ asset('/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+            <script src="{{ asset('/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+            <!-- jQuery Knob Chart -->
+            <script src="{{ asset('/plugins/knob/jquery.knob.js') }}"></script>
+            <!-- daterangepicker -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+            <script src="{{ asset('/plugins/daterangepicker/daterangepicker.js') }}"></script>
+            <!-- datepicker -->
+            <script src="{{ asset('/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+            <!-- Bootstrap WYSIHTML5 -->
+            <script src="{{ asset('/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+            <!-- Slimscroll -->
+            <script src="{{ asset('/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+            <!-- FastClick -->
+            <script src="{{ asset('/plugins/fastclick/fastclick.js') }}"></script>
+            <!-- AdminLTE App -->
+            <script src="{{ asset('/dist/js/app.min.js') }}"></script>
+            <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+            <script src="{{ asset('/dist/js/pages/dashboard.js') }}"></script>
+            <!-- AdminLTE for demo purposes -->
+            <script src="{{ asset('/dist/js/demo.js') }}"></script>
+        @show
+    </body>
+</html>
