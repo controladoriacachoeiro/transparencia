2a8ebee8d0e1cd24117a13a406b12673e27fdea2
Mateus Felicio <mateus.felicio@hotmail.com>
2017-05-24 14:39:45 -0300

    Just CSS and dates of home

--- app/Http/Controllers/ComumController.php
+++ app/Http/Controllers/ComumController.php
@@ -15,7 +15,7 @@ class ComumController extends Controller
 
         $despesaEmpenhada = [ 43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175 ];
         $despesaLiquidada = [ 13214, 42343, 43255, 53456, 65462, 53465, 54663, 65436 ];
-        $despesaPaga = [ 154175, 52503, 137133, 57177, 65462, 119931, 69658, 43934 ];
+        $despesaPaga = [ 154175, 152503, 137133, 157177, 165462, 139931, 169658, 143934 ];
 
         $receitaPrevista = 123 . ' Milhões';
 
