3d033a6682ac2531b18f9399565ec9fb74c19a23
Mateus Felicio <mateus.felicio@hotmail.com>
2017-09-12 18:16:59 -0300

    Correção de Servidores Por Cargo/Funcao e favicon

--- resources/views/api/pessoal/apiservidormatricula.blade.php
+++ resources/views/api/pessoal/apiservidormatricula.blade.php
@@ -38,7 +38,7 @@
 
                 <h3>Exemplo</h3>
                 <p><a href="/pessoal/servidores/matricula/11111">transparencia.cachoeiro.es.gov.br/api/pessoal/servidores/matricula/11111</a></p>
-                <p>Obs.: O número de inscrição utilizado acima não válido. Número utilizado apenas para ilustração
+                <p>Obs.: O número de inscrição utilizado acima não é válido. Número utilizado apenas para demonstração.
                 <h4>Retorno<h4>
                 <div class="">
                 <pre>[{"ServidorID":20357,"Matricula":"11111","CPF":"99999999999","Nome":"JOAO ","Cargo":"TECNICO INFORMATICA","Funcao":null,"TipoVinculo":"CONTRATO TEMPOR\u00c1RIO","DataExercicio":"2017-02-01","DataDemissao":"2017-12-31","Situacao":"EM EXERCICIO","OrgaoLotacao":"SEME - SECRETARIA MUNICIPAL DE EDUCA\u00c7\u00c3O","CargaHoraria":40,"Referencia":"871","Sigla":"B10","ReferenciaSigla":"871A"}]</pre>
