b52fcaae1aa37601c27ede58049bd10b39259502
Bruno <bd07gamer@gmail.com>
2017-09-20 16:10:33 -0300

    Add: tratamento para quando alguns elementos do popo up estiverem null

--- resources/views/despesas/liquidacoes/tabelaElementoDespesa.blade.php
+++ resources/views/despesas/liquidacoes/tabelaElementoDespesa.blade.php
@@ -84,17 +84,27 @@
                                             '<td>' +data[0].UnidadeGestora+ '</td>'+                                                        
                                             '</tr>'+
                                             '<tr>'+                                                        
-                                            '<td>Processo:</td>' +
-                                            '<td>' +$.trim(data[0].Processo)+ '</td>'+                                                        
-                                            '</tr>'+
-                                            '<tr>'+                                                    
+                                            '<td>Processo:</td>' ;
+                                            if((data[0].Processo=='')||(data[0].Processo==null)){
+                                                body=body+'<td>Não informado</td>';
+                                            }
+                                            else{
+                                                body=body+'<td>' +$.trim(data[0].Processo)+ '</td>';
+                                            }
+                                                                                                    
+                                body=body+'</tr>'+                                                 
                                             '<td>Ação:</td>' +
                                             '<td>' + data[0].Acao + '</td>'+                                                        
                                             '</tr>'+
                                             '<tr>'+                                                    
-                                            '<td>Subtítulo:</td>' +
-                                            '<td>' + $.trim(data[0].Subtitulo) + '</td>'+
-                                            '</tr>'+
+                                            '<td>Subtítulo:</td>';
+                                            if((data[0].Subtitulo=='')||(data[0].Subtitulo==null)){
+                                                body=body+'<td>Cachoeiro de Itapemirim</td>';
+                                            }
+                                            else{
+                                                body=body+'<td>' +$.trim(data[0].Subtitulo)+ '</td>';
+                                            }                                                 
+                                body=body+'</tr>'+
                                             '<tr>'+                                                        
                                             '<td>Elemento da Despesa:</td>' +
                                             '<td>' + data[0].ElemDespesa + '</td>'+                                                        
@@ -124,9 +134,14 @@
                                             '<td>' +stringToDate(data[0].DataLiquidacao) + '</td>'+                                                        
                                             '</tr>' +
                                             '<tr>'+                                                        
-                                            '<td>Modalidade Licitatória:</td>' +
-                                            '<td>' + $.trim(data[0].ModalidadeLicitatoria) + '</td>'+
-                                            '</tr>' +
+                                            '<td>Modalidade Licitatória:</td>';
+                                            if((data[0].ModalidadeLicitatoria=='')||(data[0].ModalidadeLicitatoria==null)){
+                                                body=body+'<td>Não Aplicável</td>';
+                                            }
+                                            else{
+                                                body=body+'<td>' +$.trim(data[0].ModalidadeLicitatoria)+ '</td>';
+                                            }     
+                                body=body+'</tr>' +
                                             '<tr>'+                                                        
                                             '<td>Categoria Econômica:</td>' +
                                             '<td>' + data[0].CatEconomica + '</td>'+                                                        
@@ -180,8 +195,6 @@
         });
     }
 </script>
-
-
 <script>
 function DownloadXls() {
 	$("#tabela").table2excel({
