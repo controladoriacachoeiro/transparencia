0de11d9a1c632e6ddda2101d5f5db5ff6df00651
Bruno <bd07gamer@gmail.com>
2017-09-26 14:05:59 -0300

    href tirado dos botões de download

--- resources/views/licitacoescontratos/tabelaProdutosAdquiridos.blade.php
+++ resources/views/licitacoescontratos/tabelaProdutosAdquiridos.blade.php
@@ -34,8 +34,8 @@
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#tab_1" data-toggle="tab" class="text-muted"><i class="fa fa-table text-purple"></i></a></li>
-                    <li><td><a href='#' id="customCSVButton"><i class='fa fa-download text-success'> CSV</i></a></li>
-                    <li><td><a href='#' id="customXLSButton"><i class='fa fa-download text-danger'> XLS</i></a></li>
+                    <li><td><a id="customCSVButton"><i class='fa fa-download text-success'> CSV</i></a></li>
+                    <li><td><a id="customXLSButton"><i class='fa fa-download text-danger'> XLS</i></a></li>
                     <li class="pull-right"><div id="chart-por-pagina"></div></li>
                     <li class="pull-right"><div id="chart-filtro"></div></li>
                 </ul>
