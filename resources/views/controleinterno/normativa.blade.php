a7e0c4b054e93e1668bd7ba9b686d2004f4b6db1
Matheus Mauricio <matheus_mauricio@hotmail.com>
2018-11-28 14:45:47 -0200

    Trocando os links http para https de Instruções Normativas

--- resources/views/controleinterno/normativa.blade.php
+++ resources/views/controleinterno/normativa.blade.php
@@ -39,10 +39,10 @@
           <div id="collapse1" class="panel-collapse collapse">
             <div class="box-body"> 
               <ul class="links-gestao">
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D240792013.html">Decreto 24079-2013 IN SCI 01-2013</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242572013.htm">Decreto 24257-2013 IN SCI 02-2013</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242582013.html">Decreto 24258-2013 IN SCO 01-2013</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242592013.htm">Decreto 24259-2013 IN SPA 01-2013</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D240792013.html">Decreto 24079-2013 IN SCI 01-2013</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242572013.htm">Decreto 24257-2013 IN SCI 02-2013</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242582013.html">Decreto 24258-2013 IN SCO 01-2013</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D242592013.htm">Decreto 24259-2013 IN SPA 01-2013</a></li>
               </ul>        
             </div>
           </div>
@@ -61,17 +61,17 @@
           <div id="collapse2" class="panel-collapse collapse">
             <div class="box-body"> 
               <ul class="links-gestao">
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244052014.html">Decreto 24405-2014 IN SCL 01-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244062014.html">Decreto 24406-2014 IN SCL 02-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244072014.html">Decreto 24407-2014 IN SCL 03-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244082014.html">Decreto 24408-2014 IN SCL 04-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244092014.html">Decreto 24409-2014 IN SCL 05-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244112014.html">Decreto 24411-2014 IN SSP 01-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244122014.html">Decreto 24412-2014 IN SSP 02-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244132014.html">Decreto 24413-2014 IN SFI 01-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248322014.html">Decreto 24832-2014 IN SAS 01-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248332014.htm">Decreto 24833-2014 IN SRH 01-2014</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248342014.htm">Decreto 24834-2014 IN STR 01-2014</a></li>                            
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244052014.html">Decreto 24405-2014 IN SCL 01-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244062014.html">Decreto 24406-2014 IN SCL 02-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244072014.html">Decreto 24407-2014 IN SCL 03-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244082014.html">Decreto 24408-2014 IN SCL 04-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244092014.html">Decreto 24409-2014 IN SCL 05-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244112014.html">Decreto 24411-2014 IN SSP 01-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244122014.html">Decreto 24412-2014 IN SSP 02-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D244132014.html">Decreto 24413-2014 IN SFI 01-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248322014.html">Decreto 24832-2014 IN SAS 01-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248332014.htm">Decreto 24833-2014 IN SRH 01-2014</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D248342014.htm">Decreto 24834-2014 IN STR 01-2014</a></li>                            
               </ul>        
             </div>
           </div>
@@ -90,9 +90,9 @@
           <div id="collapse3" class="panel-collapse collapse">
             <div class="box-body"> 
               <ul class="links-gestao">
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D256652015.html">Decreto 25665-2015 IN STI 01-2015</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D256662015.html">Decreto 25666-2015 IN SCS 01-2015</a></li>
-                <li><a target="_blank" href="http://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D257102015.html">Decreto 25710-2015 IN SSG 01-2015</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D256652015.html">Decreto 25665-2015 IN STI 01-2015</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D256662015.html">Decreto 25666-2015 IN SCS 01-2015</a></li>
+                <li><a target="_blank" href="https://leis.cachoeiro.es.gov.br:8081/normas/images/leis/html/D257102015.html">Decreto 25710-2015 IN SSG 01-2015</a></li>
               </ul>        
             </div>
           </div>
