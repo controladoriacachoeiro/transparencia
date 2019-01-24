f0be207e4b6099e8e0808d494caa24c059864663
Matheus Mauricio <matheus_mauricio@hotmail.com>
2019-01-24 10:09:45 -0200

    Adicionando um campo de pesquisa em Contratos, e alterando uma lógica de programação de Licitações

--- resources/views/licitacoescontratos/contratos/filtroContratos.blade.php
+++ resources/views/licitacoescontratos/contratos/filtroContratos.blade.php
@@ -9,14 +9,19 @@
 @stop
 
 @section('contentForm')
-    {{ Form::open(array('url' => '/licitacoescontratos/contratos', 'method' => 'POST')) }}                                                                                                
+    {{ Form::open(array('url' => '/licitacoescontratos/contratos', 'method' => 'POST')) }}
+
         <div class="row form-group">
-            <div class="col-md-4">
+            <div class="col-md-4 filtro-form">
                 {{ Form::label('Status', '', array('id'=>'lblStatus')) }}
                 {{ Form::select('slcStatus', array(), 'default', array('id'=>'slcStatus', 'class'=>'form-control')) }}
-            </div>            
-        </div>     
-                                                   
+            </div>
+            <div class="col-md-4 filtro-form">
+                {{ Form::label('Nome do Contratado / Número do Contrato / Objeto do Contrato', '', array('id'=>'lblObjeto')) }}
+                {{ Form::text('slcObjeto', '', array('id'=>'slcObjeto', 'class'=>'form-control')) }}
+            </div>
+        </div>
+
         <div class="row form-group">
             <div class="col-md-6">
                 {{ Form::submit('Pesquisar', array('class'=>'btn btn-primary')) }}
@@ -29,14 +34,14 @@
     <script src="{{ asset('/plugins/select2/select2.full.min.js') }}"></script>
     <script src="{{ asset('/js/options.min.js') }}"></script>  
     <script>
-            $(document).ready(function() {        
-                var dadosDb=<?php echo $dadosDb ?>;
-                $('#slcStatus').show();
-                $('#slcStatus').addClass("select2");
-                var select = document.getElementById("slcStatus");
-                arrayTipoConsulta2(dadosDb,select);
-                $('#slcStatus option[value="VIGENTE"]').attr("selected",true);
-                $(".select2").select2();
-             });
+        $(document).ready(function() {        
+            var dadosDb=<?php echo $dadosDb ?>;
+            $('#slcStatus').show();
+            $('#slcStatus').addClass("select2");
+            var select = document.getElementById("slcStatus");
+            arrayTipoConsulta2(dadosDb,select);
+            $('#slcStatus option[value="VIGENTE"]').attr("selected",true);
+            $(".select2").select2();
+            });
     </script>
 @endsection
